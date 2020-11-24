e618f5368d21f03644e5c11be73e205038e56603
Tim Book <timothykbook@gmail.com>
2020-11-23 20:15:35 -0500

    packaged it

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="modelmonitor",
+    author="Tim Book",
+    author_email="timothykbook@gmail.com",
+    description="A library for monitoring data changes over time",
+    url="https://github.com/timbook/modelmonitor",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
